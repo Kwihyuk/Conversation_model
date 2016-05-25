c0fd21ee60ba15105c611c8e3d0038e8cee28f11
Kwihyuk <etmoney1@gmail.com>
2016-05-26 00:59:16 +0900

    Create seq_2_seq model

--- my_conversation_model.py
+++ my_conversation_model.py
@@ -10,9 +10,25 @@ import math
 import numpy as np
 import tensorflow as tf
 import data_utils
+import seq2seq_model
 # i don't know why below import needed
 
 from six.moves import xrange
+tf.app.flags.DEFINE_float("learning_rate", 		0.5, "Learning rate.")
+tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99, "Learning rate decays by this much.")
+tf.app.flags.DEFINE_float("max_gradient_norm", 		5.0, "Clip gradients to this norm.")
+tf.app.flags.DEFINE_integer("batch_size",      		64, "Batch size to use during training.")
+tf.app.flags.DEFINE_integer("cell_size",             	1024, "Size of each model layer.")
+tf.app.flags.DEFINE_integer("num_layers", 		1, "Number of layers in the model.")
+tf.app.flags.DEFINE_integer("source_target_vocab_size",	40000, "vocabulary size.")
+tf.app.flags.DEFINE_string("data_dir", 			"../data", "Data directory")
+tf.app.flags.DEFINE_string("train_dir", 		"./train_model", "Training directory.")
+tf.app.flags.DEFINE_integer("max_train_data_size", 	0, "Limit on the size of training data (0: no limit).")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 	200, "How many training steps to do per checkpoint.")
+tf.app.flags.DEFINE_boolean("decode",			False, "Set to True for interactive decoding.")
+tf.app.flags.DEFINE_boolean("self_test", 		False, "Run a self-test if this is set to True.")
+
+FLAGS = tf.app.flags.FLAGS
 
 
 
@@ -20,7 +36,7 @@ from six.moves import xrange
 _buckets = [ (5,10), (10,15), (20,25), (30,40), (40,50) ] 
 
 
-def read_data(max_size=None): 
+def read_data(source_path,target_path, max_size=None): 
 	""" Read data from source and targt files and put into buckets.
 
 	Args:
@@ -40,10 +56,10 @@ def read_data(max_size=None):
 			         source and target are lists of token-ids.
         """
 
-	print 'a'
+	print 'extracting data'
 	data_set = [  []  for _ in _buckets ]
-	with tf.gfile.GFile('../data/Answer_token_ids.txt',mode="r") as A_file:
-		with tf.gfile.GFile('../data/Question_token_ids.txt',mode="r") as Q_file:
+	with tf.gfile.GFile(source_path,mode="r") as A_file:
+		with tf.gfile.GFile(target_path,mode="r") as Q_file:
 			source, target = A_file.readline(), Q_file.readline()
 			counter = 0
 			while source and target and ( not max_size or counter < max_size):
@@ -67,12 +83,92 @@ def read_data(max_size=None):
 
 
 
-   
+
+
+def create_model(session, forward_only):
+	""" Create conversation model and initialize or load parameter in session."""
+
+	"""Seq2SeqModel : Args
+		*source_target_vocab_size: size of the source/target vocabulary.
+		*buckets: a list of pairs (I, O), where I specifies maximum input length
+			 that will be processed in that bucket, and O specifies maximum output
+			 length. Training instances that have inputs longer than I or outputs
+			 longer than O will be pushed to the next bucket and padded accordingly.
+			 We assume that the list is sorted, e.g., [(2, 4), (8, 16)].
+		*size: number of units in each layer of the model.
+		*num_layers: number of layers in the model.
+		*max_gradient_norm: gradients will be clipped to maximally this norm.
+		*batch_size: the size of the batches used during training;
+			    the model construction is independent of batch_size, so it can be
+			    changed after initialization if this is convenient, e.g., for decoding.
+		*learning_rate: learning rate to start with.
+		*learning_rate_decay_factor: decay learning rate by this much when needed.
+		*forward_only: if set, we do not construct the backward pass in the model.
+	"""
+	model = seq2seq_model.Seq2SeqModel( FLAGS.source_target_vocab_size, _buckets, FLAGS.cell_size,
+					    FLAGS.num_layers, FLAGS.max_gradient_norm,
+					    FLAGS.batch_size, FLAGS.learning_rate,
+					    FLAGS.learning_rate_decay_factor,
+					    forward_only = forward_only)
+
+	ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
+	if ckpt and tf.gfile.Exists(ckpt.model_checkpoint_path):
+		print("Reading model parameters from %s" % ckpt.model_checkpoint_path)
+		model.saver.restore(session, ckpt.model_checkpoint_path)
+	else:
+		print("Created model with fresh parameters.")
+		session.run(tf.initialize_all_variables())
+	return model
+					    
+
+
+def train():
+   """ Train Conversation Model using Movie conversation corpus """
+
+
+   print ("Preparing move conversaion data in %s" %FLAGS.data_dir)
+	
+
+   with tf.Session() as sess:
+      # Create model
+      print ( "Creating %d layers of %d units." % (FLAGS.num_layers, FLAGS.cell_size) )
+      model = create_model(sess, False)
+
+
+      # Read data into buckets and comput their sizes.
+      print ("Reading Testing and Training data" )
+      train_set = read_data('../data/Train_Answer_token_ids.txt','../data/Train_Question_token_ids.txt')
+      test_set = read_data('../data/Test_Answer_token_ids.txt','../data/Test_Question_token_ids.txt')
+
+      train_bucket_sizes = [len(train_set[b]) for b in xrange(len(_buckets))]
+      train_total_size = float(sum(train_bucket_sizes))
+
+      print train_bucket_sizes
+      print "%d " % (train_total_size)
+
+      # A bucket scale is a list of increasing numbers from 0 to 1 that we'll use
+      # to select a bucket. Length of [scale[i], scale[i+1]] is proportional to 
+      # the size if i-th training bucket, as used later.
+
+      train_buckets_scale = [sum(train_bucket_sizes[:i+1]) / train_total_size for i in xrange(len(train_bucket_sizes))]
+
+      print train_buckets_scale
+      
+      
+
+
+
 
 
 def main(unused_args):
-	data_set = read_data()
-	print data_set[0][0]
+	train()
+#	data_set = read_data()
+#	print data_set[0][0]
+#
+#	with tf.Session() as sess:
+#		print ("Test")
+#	model = create_model(sess,False)
+#	sess.run(tf.initialize_all_variables())
 
 if __name__ == "__main__":
 	tf.app.run()
---
+++ seq2seq_model.py
@@ -0,0 +1,98 @@
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Sequence-to-sequence model with an attention mechanism."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import random
+
+import numpy as np
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+
+from tensorflow.models.rnn.translate import data_utils
+
+
+class Seq2SeqModel(object):
+  """Sequence-to-sequence model with attention and for multiple buckets.
+
+  This class implements a multi-layer recurrent neural network as encoder,
+  and an attention-based decoder. This is the same as the model described in
+  this paper: http://arxiv.org/abs/1412.7449 - please look there for details,
+  or into the seq2seq library for complete model implementation.
+  This class also allows to use GRU cells in addition to LSTM cells, and
+  sampled softmax to handle large output vocabulary size. A single-layer
+  version of this model, but with bi-directional encoder, was presented in
+    http://arxiv.org/abs/1409.0473
+  and sampled softmax is described in Section 3 of the following paper.
+    http://arxiv.org/abs/1412.2007
+  """
+
+  def __init__(self, source_target_vocab_size, buckets, size,
+               num_layers, max_gradient_norm, batch_size, learning_rate,
+               learning_rate_decay_factor, num_samples = 512, forward_only=False):
+    """Create the model.
+
+    Args:
+      source_target_vocab_size: size of the source/target vocabulary.
+      buckets: a list of pairs (I, O), where I specifies maximum input length
+        that will be processed in that bucket, and O specifies maximum output
+        length. Training instances that have inputs longer than I or outputs
+        longer than O will be pushed to the next bucket and padded accordingly.
+        We assume that the list is sorted, e.g., [(2, 4), (8, 16)].
+      size: number of units in each layer of the model.
+      num_layers: number of layers in the model.
+      max_gradient_norm: gradients will be clipped to maximally this norm.
+      batch_size: the size of the batches used during training;
+        the model construction is independent of batch_size, so it can be
+        changed after initialization if this is convenient, e.g., for decoding.
+      learning_rate: learning rate to start with.
+      learning_rate_decay_factor: decay learning rate by this much when needed.
+      forward_only: if set, we do not construct the backward pass in the model.
+    """
+    self.source_target_vocab_size = source_target_vocab_size
+    self.buckets = buckets
+    self.batch_size = batch_size
+    self.learning_rate = tf.Variable(float(learning_rate), trainable=False)
+    self.learning_rate_decay_op = self.learning_rate.assign(self.learning_rate * learning_rate_decay_factor)
+    self.global_step = tf.Variable(0, trainable=False)
+
+    # If we use sampled softmax, we need an output projection.
+    output_projection = None
+    softmax_loss_function = None
+    # Sampled softmax only makes sense if we sample less than vocabulary size.
+    if num_samples > 0 and num_samples < self.source_target_vocab_size:
+      with tf.device("/cpu:0"):
+        w = tf.get_variable("proj_w", [size, self.source_target_vocab_size])
+        w_t = tf.transpose(w)
+        b = tf.get_variable("proj_b", [self.source_target_vocab_size])
+      output_projection = (w, b)
+
+      def sampled_loss(inputs, labels):
+        with tf.device("/cpu:0"):
+          labels = tf.reshape(labels, [-1, 1])
+          return tf.nn.sampled_softmax_loss(w_t, b, inputs, labels, num_samples,
+                                            self.target_vocab_size)
+      softmax_loss_function = sampled_loss
+
+    # Create the internal multi-layer cell for our RNN.
+    single_cell = tf.nn.rnn_cell.BasicLSTMCell(size)
+    cell = single_cell
+    if num_layers > 1:
+      cell = tf.nn.rnn_cell.MultiRNNCell([single_cell] * num_layers)
+
